f35683211ea78a82731093a1de83eb3087088b3f
Joshua T J <joshuajose312@gmail.com>
2024-03-08 12:29:46 +0530

    unit added to localStorage

--- src/app/Components/weather/weather.component.ts
+++ src/app/Components/weather/weather.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { WeatherService } from '../../Services/weather.service';
 import { DatePipe } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -11,33 +16,51 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   standalone: true,
   imports: [DatePipe, ReactiveFormsModule, RouterLink, MatTooltipModule],
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
-
   weatherData: any;
   unit: string = 'metric';
   date: Date = new Date();
 
-  constructor(private weatherService: WeatherService, private fb: FormBuilder) { }
+  constructor(private weatherService: WeatherService, private fb: FormBuilder) {
+    this.getLocalStorageValue();
+    this.weatherUnit.valueChanges.subscribe((value) => {
+      this.setUnit(value);
+    });
+  }
+
+  weatherUnit = new FormControl(false);
 
   formWeather = this.fb.group({
-    Location: ['', Validators.required]
-  })
+    Location: ['', Validators.required],
+  });
+
+  getLocalStorageValue() {
+    let unit = localStorage.getItem('unit');
+    let booleanUnit = unit === 'true';
+    this.unit = booleanUnit ? 'imperial' : 'metric';
+    this.weatherUnit.setValue(booleanUnit);
+  }
 
   getWeather() {
     if (this.formWeather.valid) {
-      this.weatherService.getCityWeather(this.formWeather.controls.Location.value || '', this.unit).subscribe({
-        next: (res) => {
-          console.log(res)
-          this.weatherData = res;
-        },
-        error: (error: HttpErrorResponse) => {
-          if (error.status === 404) {
-            alert(error.error?.message)
-          }
-        }
-      })
+      this.weatherService
+        .getCityWeather(
+          this.formWeather.controls.Location.value || '',
+          this.unit
+        )
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.weatherData = res;
+          },
+          error: (error: HttpErrorResponse) => {
+            if (error.status === 404) {
+              alert(error.error?.message);
+            }
+          },
+        });
     }
   }
 
@@ -46,10 +69,8 @@ export class WeatherComponent {
   }
 
   setUnit(event: any) {
-    if (event) {
-      this.unit = event.target.checked ? 'imperial' : 'metric';
-    }
+    localStorage.setItem('unit', event);
+    this.unit = event ? 'imperial' : 'metric';
     this.getWeather();
   }
-
 }
